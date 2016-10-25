5ab645b3c2b426b75a7eba6504c0edaa0a7571f3
M <myeveryheart@qq.com>
2016-10-25 19:42:18 +0800

    修复uri为nil时闪退

--- Cenarius/Core/CNRSRoute.m
+++ Cenarius/Core/CNRSRoute.m
@@ -8,18 +8,26 @@
 
 #import "CNRSRoute.h"
 #import "CNRSConfig.h"
+#import "CNRSLogging.h"
 
 @implementation CNRSRoute
 
 - (instancetype)initWithDictionary:(NSDictionary *)dict
 {
-  if ( (self = [super init]) ) {
-      
-    _uri = [NSURL URLWithString:dict[@"file"]];
-      _remoteHTML = [[CNRSConfig remoteFolderUrl] URLByAppendingPathComponent:_uri.absoluteString];
-      _fileHash = dict[@"hash"];
-  }
-  return self;
+    if ( (self = [super init]) )
+    {
+        _fileHash = dict[@"hash"];
+        _uri = [NSURL URLWithString:dict[@"file"]];
+        if (_uri.absoluteString != nil)
+        {
+            _remoteHTML = [[CNRSConfig remoteFolderUrl] URLByAppendingPathComponent:_uri.absoluteString];
+        }
+        else
+        {
+            CNRSErrorLog(@"file in route %@ is nil", dict);
+        }
+    }
+    return self;
 }
 
 @end
