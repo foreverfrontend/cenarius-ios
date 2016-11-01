7a94f8e2fc7ced8951f2afaa95035b29afd365d9
M <myeveryheart@qq.com>
2016-11-01 19:45:59 +0800

    no message

--- Cenarius/Core/CNRSLogging.h
+++ Cenarius/Core/CNRSLogging.h
@@ -6,12 +6,20 @@
 //  Copyright © 2016年 M. All rights reserved.
 //
 
+#import "CNRSConfig.h"
+
 #ifdef DEBUG
-#define CNRSLog(...) NSLog(@"[Cenarius] " __VA_ARGS__)
+#define CNRSLog(format, ...) do {                                                                           \
+                                fprintf(stderr, "<%s : %d> %s\n",                                           \
+                                [[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String],  \
+                                __LINE__, __func__);                                                        \
+                                (NSLog)((format), ##__VA_ARGS__);                                           \
+                                fprintf(stderr, "-------\n");                                               \
+                                } while (0)
 #else /* DEBUG */
-#define CNRSLog(...)
+#define CNRSLog(format, ...) do {} while (0)
 #endif /* DEBUG */
 
-#define CNRSDebugLog(...)  CNRSLog(@"[DEBUG] " __VA_ARGS__)
-#define CNRSWarnLog(...)   CNRSLog(@"[WARN] " __VA_ARGS__)
-#define CNRSErrorLog(...)  CNRSLog(@"[ERROR] " __VA_ARGS__)
+#define CNRSDebugLog(format, ...)  CNRSLog(@"[DEBUG] " __VA_ARGS__)
+#define CNRSWarnLog(format, ...)   CNRSLog(@"[WARN] " __VA_ARGS__)
+#define CNRSErrorLog(format, ...)  CNRSLog(@"[ERROR] " __VA_ARGS__)
