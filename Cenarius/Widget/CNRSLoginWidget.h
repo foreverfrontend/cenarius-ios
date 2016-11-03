df6f70662e32a3cb32d577f329c5858ec124b3da
M <myeveryheart@qq.com>
2016-11-03 17:40:17 +0800

    登录

--- Cenarius/Widget/CNRSLoginWidget.h
+++ Cenarius/Widget/CNRSLoginWidget.h
@@ -12,5 +12,5 @@
  * `CNRSLoginWidget` 实现登录。
  */
 @interface CNRSLoginWidget : NSObject<CNRSWidget>
-
++ (void)createAccessTokenWithUsername:(NSString *)username password:(NSString *)password;
 @end
--- Cenarius/Widget/CNRSLoginWidget.m
+++ Cenarius/Widget/CNRSLoginWidget.m
@@ -9,6 +9,13 @@
 #import "CNRSLoginWidget.h"
 #import "CNRSViewController.h"
 #import "AFNetworking.h"
+#import <CommonCrypto/CommonDigest.h>
+
+#define kService @"https://uim-test.infinitus.com.cn/oauth20/accessToken"
+//#define kService @"http://172.21.29.53:8080/macula-uim-webapp/oauth20/accessToken"
+#define kTerminalType @"mobile"
+#define kAppKey @"BUPM"
+#define kAppSecret @"rfGd23Yhjd92JkpWe"
 
 @interface CNRSLoginWidget ()
 
@@ -42,21 +49,83 @@
     
 }
 
-- (void)createAccessTokenWithUsername:(NSString *)username password:(NSString *)password
++ (void)createAccessTokenWithUsername:(NSString *)username password:(NSString *)password
 {
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
+    parameters[@"app_key"] = kAppKey;
+    parameters[@"timestamp"] = [NSNumber numberWithInteger:[NSDate date].timeIntervalSince1970 * 1000];
     parameters[@"username"] = username;
     parameters[@"password"] = password;
-    parameters[@"terminalType"] = @"terminalType";
-    parameters[@"remenberMe"] = @"remenberMe";
-
-    [manager POST:@"https://uim-test.infinitus.com.cn/oauth20/accessToken" parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        
+    parameters[@"terminalType"] = kTerminalType;
+    parameters[@"remenberMe"] = @"true";
+    
+    NSString *sign = [self md5Signature:parameters secret:kAppSecret];
+    parameters[@"sign"] = sign;
+    [manager POST:kService parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSString *token = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+//        NSLog(@"%@",token);
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         
     }];
 
+}
+
+/**
+ * 新的md5签名，首尾放secret。
+ *
+ * @param params 传给服务器的参数
+ * @param secret 分配给您的APP_SECRET
+ */
++(NSString *)md5Signature:(NSDictionary *)params secret:(NSString *)secret
+{
+    NSString *result = nil;
+    NSString *orgin = [self getBeforeSign:params orgin:secret];
+    if (orgin == nil)
+    {
+        return nil;
+    }
+    // secret last
+    orgin = [orgin stringByAppendingString:secret];
+    result = [self md5:orgin];
+    
+    return result;
+}
+
+/**
+ * 添加参数的封装方法
+ *
+ * @param params
+ * @param orgin
+ * @return
+ */
++(NSString *)getBeforeSign:(NSDictionary *)params orgin:(NSString *)orgin
+{
+    NSArray *keys = params.allKeys;
+    NSArray *newKeys = [keys sortedArrayUsingSelector:@selector(compare:)];
+    for (NSString *key in newKeys)
+    {
+        orgin = [orgin stringByAppendingFormat:@"%@%@",key,params[key]];
+    }
+    
+    return orgin;
+}
+
+/**
+ *  生成字符串的Md5值
+ */
++ (NSString *)md5:(NSString *)str
+{
+    const char *cStr = [str UTF8String];
+    unsigned char result[16];
+    CC_MD5( cStr, strlen(cStr), result );
+    return [NSString stringWithFormat:@"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+            result[0], result[1], result[2], result[3],
+            result[4], result[5], result[6], result[7],
+            result[8], result[9], result[10], result[11],
+            result[12], result[13], result[14], result[15]
+            ];
 }
 
 @end
