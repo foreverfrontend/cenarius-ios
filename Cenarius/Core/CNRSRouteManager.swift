14605cb2a7cb64914175526f8629772b887f3fa2
M <myeveryheart@qq.com>
2017-03-27 18:03:53 +0800

    CNRSRouteManager

--- Cenarius/Core/CNRSRouteManager.swift
+++ Cenarius/Core/CNRSRouteManager.swift
@@ -12,6 +12,25 @@ import Alamofire
 /// `CNRSRouteManager` 提供了对路由信息的管理和使用接口。
 class CNRSRouteManager {
     
+    private static let sharedInstance = CNRSRouteManager()
+    /// www文件夹的url
+    private var wwwUrl: URL!
+    private var developMode = true
+    typealias Completion = (State, Int) -> Void
+    private var completion: Completion!
+    
+    enum State {
+        case UNZIP_WWW//解压www
+        case UNZIP_WWW_ERROR//解压www出错
+        case DOWNLOAD_CONFIG//下载配置文件
+        case DOWNLOAD_CONFIG_ERROR//下载配置文件出错
+        case DOWNLOAD_ROUTES//下载路由表
+        case DOWNLOAD_ROUTES_ERROR//下载路由表出错
+        case DOWNLOAD_FILES//下载文件
+        case DOWNLOAD_FILES_ERROR//下载文件出错
+        case UPDATE_SUCCESS//更新文件成功
+    }
+    
     /// 设置远程资源地址。
     ///
     /// - Parameter url: www文件夹的url
@@ -26,28 +45,16 @@ class CNRSRouteManager {
     /// 更新
     ///
     /// - Parameter completionHandler: 回调
-    class func update(completionHandler: Completion)  {
-       
+    class func update(completionHandler: @escaping Completion)  {
+        sharedInstance.update(completionHandler: completionHandler)
     }
     
-    private static let sharedInstance = CNRSRouteManager()
-    /// www文件夹的url
-    private var wwwUrl: URL!
-    private var developMode = false
-    typealias Completion = (State, Int) -> Void
-    private var completion: Completion!
-    
-    
-    enum State {
-        case UNZIP_WWW//解压www
-        case UNZIP_WWW_ERROR//解压www出错
-        case DOWNLOAD_CONFIG//下载配置文件
-        case DOWNLOAD_CONFIG_ERROR//下载配置文件出错
-        case DOWNLOAD_ROUTES//下载路由表
-        case DOWNLOAD_ROUTES_ERROR//下载路由表出错
-        case DOWNLOAD_FILES//下载文件
-        case DOWNLOAD_FILES_ERROR//下载文件出错
-        case UPDATE_SUCCESS//更新文件成功
+    func update(completionHandler: @escaping Completion)  {
+        completion = completionHandler
+        // 开发模式，直接成功
+        if developMode {
+            completion(.UPDATE_SUCCESS, 100)
+        }
     }
     
 }
